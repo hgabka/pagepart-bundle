4760eabb8dda4a8eaf50ea8726d17ec147a312a8
hgabka <hgabka@gmail.com>
2020-02-01 15:26:43 +0100

    Update LinePagePart.php

--- src/Entity/LinePagePart.php
+++ src/Entity/LinePagePart.php
@@ -26,7 +26,7 @@ class LinePagePart extends AbstractPagePart
      */
     public function getDefaultView()
     {
-        return 'HgabkaPagePartBundle:LinePagePart:view.html.twig';
+        return '@HgabkaPagePart/LinePagePart/view.html.twig';
     }
 
     /**
