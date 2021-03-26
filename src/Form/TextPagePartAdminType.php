f3ebf455132ea751754534a315499660b8971089
hgabka <hgabka@gmail.com>
2021-03-26 05:59:26 +0100

    Update TextPagePartAdminType.php

--- src/Form/TextPagePartAdminType.php
+++ src/Form/TextPagePartAdminType.php
@@ -30,6 +30,10 @@ class TextPagePartAdminType extends AbstractPagePartAdminType
                 'contentsCss' => $options['config']['editorCss'],
             ];
         }
+                        
+        if (isset($options['config']['editor_mode'])) {
+            $params['attr']['type'] = $options['config']['editor_mode'];
+        }
 
         $builder->add('content', WysiwygType::class, $params);
     }
