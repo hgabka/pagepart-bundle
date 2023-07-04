4e8a364f54f245eebb61e602beabc9e9c31c9fa8
hgabka <hgabka@gmail.com>
2023-07-04 11:06:50 +0200

    Update RawHTMLPagePartAdminType.php

--- src/Form/RawHTMLPagePartAdminType.php
+++ src/Form/RawHTMLPagePartAdminType.php
@@ -16,7 +16,7 @@ class RawHTMLPagePartAdminType extends AbstractPagePartAdminType
      * @param \Symfony\Component\Form\FormBuilderInterface $builder
      * @param array                                        $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add('content', TextareaType::class, [
             'label' => 'pagepart.html.content',
@@ -31,7 +31,7 @@ class RawHTMLPagePartAdminType extends AbstractPagePartAdminType
     /**
      * @return string
      */
-    public function getBlockPrefix()
+    public function getBlockPrefix(): string
     {
         return 'hgabka_pagepartbundle_rawhtmlpageparttype';
     }
@@ -39,7 +39,7 @@ class RawHTMLPagePartAdminType extends AbstractPagePartAdminType
     /**
      * @param \Symfony\Component\OptionsResolver\OptionsResolver $resolver
      */
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         parent::configureOptions($resolver);
         $resolver->setDefaults([
