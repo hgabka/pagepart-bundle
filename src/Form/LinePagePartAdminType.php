8f685135a1eb07be5f9f1c2f6513dd745901b8d2
hgabka <hgabka@gmail.com>
2023-07-04 11:06:12 +0200

    Update LinePagePartAdminType.php

--- src/Form/LinePagePartAdminType.php
+++ src/Form/LinePagePartAdminType.php
@@ -15,14 +15,14 @@ class LinePagePartAdminType extends AbstractPagePartAdminType
      * @param \Symfony\Component\Form\FormBuilderInterface $builder
      * @param array                                        $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
     }
 
     /**
      * @return string
      */
-    public function getBlockPrefix()
+    public function getBlockPrefix(): string
     {
         return 'hgabka_pagepartbundle_linepageparttype';
     }
@@ -30,7 +30,7 @@ class LinePagePartAdminType extends AbstractPagePartAdminType
     /**
      * @param \Symfony\Component\OptionsResolver\OptionsResolver $resolver
      */
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         parent::configureOptions($resolver);
         $resolver->setDefaults(
